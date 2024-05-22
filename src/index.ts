af1fc0086cb9f534bcb7fd466665ed50ba4df1ca
Cheng <mazeyqian@gmail.com>
2024-05-22 10:20:19 +0800

    feat: add isMiddleScreen and isLongScreen

--- src/index.ts
+++ src/index.ts
@@ -14,20 +14,6 @@ export const quickToast = (msg: string): void => {
   });
 };
 
-export const isH5 = (): boolean => {
-  return process.env.TARO_ENV === 'h5';
-};
-
-export const isPC = (): boolean => {
-  if (isH5()) {
-    const info = getBrowserInfo();
-    if (info.platform === 'desktop') {
-      return true;
-    }
-  }
-  return false;
-};
-
 export const getEnv = (): string => {
   return process.env.TARO_ENV || '';
 };
@@ -56,4 +42,36 @@ export const getWindowSize = () => {
   };
 };
 
-// todo: isMiddleScreen, isLongScreen
+export const isWeb = (): boolean => {
+  return process.env.TARO_ENV === 'h5';
+};
+
+export const isH5 = (): boolean => {
+  return isWeb();
+};
+
+export const isPC = (): boolean => {
+  if (isH5()) {
+    const info = getBrowserInfo();
+    if (info.platform === 'desktop') {
+      return true;
+    }
+  }
+  return false;
+};
+
+export const isMiddleScreen = (): boolean => {
+  const { ratio } = getWindowSize();
+  if (ratio > 0.7) {
+    return true;
+  }
+  return false;
+};
+
+export const isLongScreen = (): boolean => {
+  const { hwRatio } = getWindowSize();
+  if (hwRatio > 1.9) {
+    return true;
+  }
+  return false;
+};
